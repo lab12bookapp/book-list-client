87603c04c2b425b7dd8e72f0a46673730fb3c6cf
Haley Mendoza <mendozahaley@gmail.com>
2018-04-21 15:28:12 -0700

    changed heroku url.

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,7 +5,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://jl-tc-pk-booklist.herokuapp.com/';
+ENV.productionApiUrl = 'https://lm-hm-booklist.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
